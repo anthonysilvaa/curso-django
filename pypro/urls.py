205f6b540771f275425c41f822dc9c9c5ed52c26
Anthony Silva <hedreson@icloud.com>
2022-05-04 12:40:16 -0300

    Configurado django-debug-toolbar 1

--- pypro/urls.py
+++ pypro/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
 from pypro.base.views import home
 
 urlpatterns = [

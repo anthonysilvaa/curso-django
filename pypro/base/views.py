52fa7ced738e197eee3b3f18f47945852ff61ec9
Anthony Silva <hedreson@icloud.com>
2022-05-06 17:36:43 -0300

    Interface implementada com Layoutit e Bootstrap

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
 from django.shortcuts import render
+
 
 def home(request):
     # raise ValueError()

c37efaa79b2f3b535213c3046576e98f8089e434
Anthony Silva <hedreson@icloud.com>
2022-05-08 18:47:17 -0300

    Criada Landing Page

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,16 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from functools import partial
-from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 

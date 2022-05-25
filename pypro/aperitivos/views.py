f080f06453ade4dcb85dabf576ccaf0bc17e6eb6
Anthony Silva <hedreson@icloud.com>
2022-05-25 02:03:13 -0300

    Aplicando alterações de vídeos

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+#Contexto do template
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+
+    videos = {
+
+       'motivacao':{'titulo': 'Vídeo SoloLeveling', 'vimeo_id': 713312782},
+        'future-royalty': {'titulo': 'Vídeo Future Royalty', 'vimeo_id': 713543859}
+
+    }
+
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})

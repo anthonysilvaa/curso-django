fd9cd745f8362e905f8d13282549cf98fc453fdc
Anthony Silva <hedreson@icloud.com>
2022-05-25 02:07:36 -0300

    Corrigindo erros apontados por flake8

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Contexto do template
 
-#Contexto do template
+
 def video(request, slug):
 
     videos = {
 
-       'motivacao':{'titulo': 'Vídeo SoloLeveling', 'vimeo_id': 713312782},
-        'future-royalty': {'titulo': 'Vídeo Future Royalty', 'vimeo_id': 713543859}
+       'motivacao': {'titulo': 'Vídeo SoloLeveling', 'vimeo_id': 713312782},
+       'future-royalty': {'titulo': 'Vídeo Future Royalty', 'vimeo_id': 713543859}
 
     }
 
